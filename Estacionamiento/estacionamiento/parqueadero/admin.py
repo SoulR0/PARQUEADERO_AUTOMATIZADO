8cbd5f30c7f106c1a05d1b3d9af7f5277e7a9a48
MOI1977 <ymsalgadobrito@gmail.com>
2025-05-14 19:26:49 -0500

    Descripción del cambio

--- Estacionamiento/estacionamiento/parqueadero/admin.py
+++ Estacionamiento/estacionamiento/parqueadero/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Zona, Vehiculo, RegistroParqueo, Tarifa
+
+admin.site.register(Zona)
+admin.site.register(Vehiculo)
+admin.site.register(RegistroParqueo)
+admin.site.register(Tarifa)
