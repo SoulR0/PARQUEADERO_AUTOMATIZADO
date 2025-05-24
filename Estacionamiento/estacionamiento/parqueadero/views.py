edcf56111497f58afb52b360c5a89be490929d03
gustavo4538 <gustavochoa62@gmail.com>
2025-05-23 19:55:13 -0500

    Aporte en los sistemas de bienvenida

--- Estacionamiento/estacionamiento/parqueadero/views.py
+++ Estacionamiento/estacionamiento/parqueadero/views.py
@@ -2,13 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def mi_vista(request):
-    mensaje = """
-    ¡Bienvenido al sistema de parqueadero!<br>
-    Estacionamiento exclusivo para clientes autorizados.<br>
-    Zona de estacionamiento vigilada. No nos hacemos responsables por objetos personales.<br>
-    Respetar los espacios señalados. Gracias por su cooperación.
-    """
-    return HttpResponse(mensaje)
+    return HttpResponse("¡Bienvenido al sistema de parqueadero!")
 
 from django.shortcuts import render
 from .models import Vehiculo
@@ -16,4 +10,3 @@ from .models import Vehiculo
 def lista_vehiculos(request):
     vehiculos = Vehiculo.objects.all()
     return render(request, 'vehiculos/lista_vehiculos.html', {'vehiculos': vehiculos})
-
